a9d0b1473d26bc4850fa82432525e7465d0efacc
caitlin <113788299+alaskasalad@users.noreply.github.com>
2025-09-25 15:54:40 -0400

    changed how long it goes for

--- haskell/assignment1/main.hs
+++ haskell/assignment1/main.hs
@@ -1,25 +1,28 @@
 -- Assignment Haskell, Caitlin Box 
 -- made 9/25/25 
 
--- short Pi:
+-- short Pi, just did this to practice before the others
 shortPi :: Double
 shortPi = 16 * atan (1/5) - 4 * atan (1/239)
 
 -- Simple Pi:
+-- i had to change how many times it summed up bc it wasn't as accurate
 simplePi :: Double
-simplePi = 4 * sum [((-1) ^ (x+1)) / fromIntegral (2*x - 1) | x <- [1..1000000]]
+simplePi = 4 * sum [((-1) ^ (x+1)) / fromIntegral (2*x - 1) | x <- [1..700]]
 
 -- Wells Pi
 wellsPi :: Double
-wellsPi = sqrt (8 * sum [1 / (2*x -1)^2 | x <- [1..1000000]])
+wellsPi = sqrt (8 * sum [1 / (2*x -1)^2 | x <- [1..200]])
 
--- Euler Pi 
+-- Euler Pi
+-- i had to change how many times it summed up bc it wasn't as accurate
 eulerPi :: Double
-eulerPi = sqrt (6 * sum [1/x^2 | x <- [1..1000000]])
+eulerPi = sqrt (6 * sum [1/x^2 | x <- [1..600]])
 
 -- Wallis Formula 
+-- i had to change how many times it summed up bc it wasn't as accurate
 wallisFormula :: Double
-wallisFormula = 2 * product [(2*x)^2 / ((2*x -1)*(2*x +1)) | x <- [1..1000000]]
+wallisFormula = 2 * product [(2*x)^2 / ((2*x -1)*(2*x +1)) | x <- [1..500]]
 
 main :: IO()
 main = do
