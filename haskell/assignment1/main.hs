857444aabd35e551ec2a890bdb0f64967116777d
caitlin <113788299+alaskasalad@users.noreply.github.com>
2025-09-25 15:45:45 -0400

    pi assignment done

--- haskell/assignment1/main.hs
+++ haskell/assignment1/main.hs
@@ -1,18 +1,25 @@
 -- Assignment Haskell, Caitlin Box 
+-- made 9/25/25 
 
 -- short Pi:
-shortPi :: Floating a => a
+shortPi :: Double
 shortPi = 16 * atan (1/5) - 4 * atan (1/239)
 
 -- Simple Pi:
-simplePi = 4 * sum [((-1) ^ x+1) / fromIntegral (2*x - 1) | x <- [1..1000000]]
+simplePi :: Double
+simplePi = 4 * sum [((-1) ^ (x+1)) / fromIntegral (2*x - 1) | x <- [1..1000000]]
 
 -- Wells Pi
+wellsPi :: Double
+wellsPi = sqrt (8 * sum [1 / (2*x -1)^2 | x <- [1..1000000]])
 
 -- Euler Pi 
+eulerPi :: Double
 eulerPi = sqrt (6 * sum [1/x^2 | x <- [1..1000000]])
 
 -- Wallis Formula 
+wallisFormula :: Double
+wallisFormula = 2 * product [(2*x)^2 / ((2*x -1)*(2*x +1)) | x <- [1..1000000]]
 
 main :: IO()
 main = do
@@ -21,15 +28,17 @@ main = do
     putStrLn ""
 
     putStrLn "Wells Pi"
+    print wellsPi
     putStrLn ""
 
-    putStrLn "short Pi"
+    putStrLn "Short Pi"
     print shortPi
     putStrLn ""
 
-    putStrLn "euler Pi"
+    putStrLn "Euler Pi"
     print eulerPi
     putStrLn ""
 
-    putStrLn "wallisSum Pi"
+    putStrLn "WallisSum Pi"
+    print wallisFormula
     putStrLn ""
